d3032be8cf12919365ebd6687a974ce7d9295462
Arpit Kahale <arpitkahalek13@gmail.com>
2026-04-13 17:56:23 -0700

    fix production API URL

--- frontend/password-reset-frontend/src/services/authService.js
+++ frontend/password-reset-frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/auth";
+const API = "https://password-reset-y3qu.onrender.com/api/auth";
 
 export const forgotPassword = (email) => {
   return axios.post(`${API}/forgot-password`, { email });
